8fbb351c7ccaac47bc1d491ebccd949af4260d57
Ratul sharker <sharker.ratul.08@gmail.com>
2016-05-24 09:44:31 +0600

    KeyboardAnimatorDelegate is added with an example.

---
+++ Example Project/Testing/Testing/UsingKeyboardAnimatorDelegateViewController.h
@@ -0,0 +1,13 @@
+//
+//  UsingKeyboardAnimatorDelegateViewController.h
+//  KeyboardAnimator
+//
+//  Created by Ratul Sharker on 5/22/16.
+//  Copyright © 2016 revesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsingKeyboardAnimatorDelegateViewController : UIViewController
+
+@end
---
+++ Example Project/Testing/Testing/UsingKeyboardAnimatorDelegateViewController.m
@@ -0,0 +1,13 @@
+//
+//  UsingKeyboardAnimatorDelegateViewController.m
+//  KeyboardAnimator
+//
+//  Created by Ratul Sharker on 5/22/16.
+//  Copyright © 2016 revesoft. All rights reserved.
+//
+
+#import "UsingKeyboardAnimatorDelegateViewController.h"
+
+@implementation UsingKeyboardAnimatorDelegateViewController
+
+@end
